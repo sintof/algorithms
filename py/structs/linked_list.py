16fc608e7075aed7d75d91c91d2c59ccd57d87e5
sintof <dcpersonf@gmail.com>
2024-09-08 23:32:14 +0500

    Sort added to Linked List

--- py/structs/linked_list.py
+++ py/structs/linked_list.py
@@ -8,11 +8,12 @@ class Node:
 
 
     def __repr__(self):
-        s = ""
         current = self
+        s = ""
         while current:
-            s += str(current.data) + " "
+            s += str(current.data) + " -> "
             current = current.next
+        s += "None"
         return s
 
 
@@ -41,10 +42,54 @@ def delete_node(head, key):
     return head
 
 
+@timeit
+def search(head, key):
+    current = head
+    while current:
+        if current.data == key:
+            return True
+        current = current.next
+    return False
 
 
+@timeit
+def reverse(head):
+    prev = None
+    cur = head
+    while cur:
+        next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next
+    return prev
 
-head = Node(1, Node(2, Node(3, Node(4, Node(5, Node(6, Node(7, Node(8))))))))
+
+@timeit
+def detect_loop(head):
+    slow = head
+    fast = head
+    while fast and slow and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+
+@timeit
+def bubble_sort(head):
+    cur = head
+    while cur:
+        runner = cur
+        while runner.next:
+            if runner.data > runner.next.data:
+                runner.data, runner.next.data = runner.next.data, runner.data
+            runner = runner.next
+        cur = cur.next
+    return head
+
+
+head = Node(1, Node(2, Node(4)))
 # head = Node(1)
 
 
@@ -52,8 +97,16 @@ def start():
     global head
     print(head)
 
-    head = insert_end(head, 9)
+    head = insert_end(head, 3)
     print(head)
 
-    head = delete_node(head, 9)
+    head = bubble_sort(head)
     print(head)
+    print(detect_loop(head))
+
+    head = delete_node(head, 2)
+    print(head)
+
+    print(search(head, 3))
+    print(search(head, 9))
+    print(reverse(head))
