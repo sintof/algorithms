5f70cf8985a77e8d267b06ce63098e4b90cda7b6
sintof <dcpersonf@gmail.com>
2024-09-10 23:16:23 +0500

    Added cpp sort algorithm

---
+++ cpp/algos/sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+#define endl "\n"
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    cout << "Sorted array: ";
+    printArray(arr, n);
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+
+    return 0;
+}
